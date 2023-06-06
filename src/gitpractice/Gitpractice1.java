13bc3fdb6517f97e8db01bbf38106fb14a9d481f
BIYYANI ARUN KUMAR REDDY <arunreddy453@gmail.com>
2023-06-06 12:46:54 +0530

    git practice eclipse

---
+++ src/gitpractice/Gitpractice1.java
@@ -0,0 +1,18 @@
+package gitpractice;
+
+public class Gitpractice1 {
+	
+	public void add()  {
+		int a = 48;
+		int b = 89;
+		int c = a+b;
+		
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) {
+		Gitpractice1 g = new Gitpractice1();
+		g.add();
+	}
+
+}
